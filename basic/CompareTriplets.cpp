ae6bfb9a00d7c13291a2aede9136b69200c717d8
NaumaanSaifi1 <153414153+NaumaanSaifi1@users.noreply.github.com>
2024-11-03 15:27:04 +0530

    Create CompareTriplets.cpp

---
+++ basic/CompareTriplets.cpp
@@ -0,0 +1,14 @@
+vector<int> compareTriplets(vector<int> a, vector<int> b) {
+int alice ,bob;
+alice = 0;
+bob = 0;
+vector<int>result;
+for (int i = 0; i<3 ; i++){
+    if (a[i]<b[i]){
+        bob++;
+    }
+    else if (a[i]>b[i]){
+        alice++;
+    }
+    else;
+}
